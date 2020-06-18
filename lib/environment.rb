a97c5fb126b2d502c2a27127ea072c093d6891d6
dwandrew <danwandrew@gmail.com>
2020-06-18 16:31:10 +0100

    renamed a few base files

--- lib/environment.rb
+++ lib/environment.rb
@@ -13,8 +13,8 @@ require_relative './Dnd/group_spells.rb'
 require_relative './Dnd/monsters.rb'
 require_relative './Dnd/single_monster.rb'
 require_relative './Dnd/group_monsters.rb'
-require_relative './Dnd/monster_menu.rb'
-require_relative './Dnd/spell_menu.rb'
+require_relative './Dnd/monster_cli.rb'
+require_relative './Dnd/spell_cli.rb'
 
 
 
